f7b0403ce005e9c76d6e7e3f359b93359b4f32ad
Daniel Nobrega <nobregad72@gmail.com>
2024-10-04 11:00:49 -0300

    PROJETO ATUALIZADO

--- script.js
+++ script.js
@@ -22,6 +22,25 @@ const weapons = [
   { name: 'claw hammer', power: 50 },
   { name: 'sword', power: 100 }
 ];
+
+const monsters = [
+  {
+    name: "slime",
+    level: 2,
+    health: 15
+  },
+  {
+    name: "fanged beast",
+    level: 8,
+    health: 60
+  },
+  {
+    name: "dragon",
+    level: 20,
+    health: 300
+  }
+];
+
 const location = [
   {
     name: "town square",
@@ -40,6 +59,12 @@ const location = [
     "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
     "button functions": [fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters."
+  },
+  {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster."
   }
 ];
 
@@ -67,12 +92,9 @@ function goStore() {
 }
   
   function goCave() {
-    console.log("Going to cave.");
+    update(locations[2]);
   }
 
-  function fightDragon() {
-    console.log("Fighting dragon.");
-  }
   
   function buyHealth() {
     if (gold >= 10) {
@@ -84,20 +106,88 @@ function goStore() {
       text.innerText = "You do not have enough gold to buy health.";
     }
   }
-
+  
   function buyWeapon() {
-    if (gold >= 30) {
-      gold -= 30;
-      currentWeaponIndex++;
-      goldText.innerText = gold;
-      let newWeapon = weapons;
-      text.innerText = "You now have a new weapon.";
+    if (currentWeaponIndex < weapons.length - 1) {
+      if (gold >= 30) {
+        gold -= 30;
+        currentWeaponIndex++;
+        goldText.innerText = gold;
+        let newWeapon = weapons[currentWeaponIndex].name;
+        text.innerText = "You now have a " + newWeapon + ".";
+        inventory.push(newWeapon);
+        text.innerText += " In your inventory you have: " + inventory;
+      } else {
+        text.innerText = "You do not have enough gold to buy a weapon.";
+      }
+    } else {
+      text.innerText = "You already have the most powerful weapon!";
+      button2.innerText = "Sell weapon for 15 gold";
+      button2.onclick = sellWeapon;
     }
   }
-  function fightSlime() {
 
+  function sellWeapon() {
+    if (inventory.length > 1) {
+      gold += 15;
+      goldText.innerText = gold;
+      let currentWeapon = inventory.shift();
+      text.innerText = "You sold a " + currentWeapon + ".";
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "Don't sell your only weapon!";
+    }
+  }
+
+  function fightSlime() {
+    fighting = 0;
+    goFight();
   }
   
   function fightBeast() {
+    fighting = 1;
+    goFight();
+  }
   
+  function fightDragon() {
+    fighting = 2;
+    goFight();
+  }
+
+  function goFight() {
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth;
+  }
+  
+  function attack() {
+    text.innerText = "The " + monsters[fighting].name + " attacks.";
+    text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+    healthText.innerText = health;
+    monsterHealthText.innerText = monsterHealth;
+    if (health <= 0) {
+      lose();
+    } else if (monsterHealth <= 0) {
+      defeatMonster();
+    }
+  }
+  
+  function dodge() {
+    text.innerText = "You dodge the attack from the " + monsters[fighting].name; //monsters[fighting].name vai puxar o nome do monstro que vc está lutando 
+  }
+
+  function defeatMonster() {
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level; //monsters[fighting].level exibe o nivel do monstro que vc está lutando.
+    goldText.innerText = gold; //Exibe o valor do ouro atualizado.
+    xpText.innerText = xp; //Exibe o valor do xp atualizado.
+    
+  }
+
+  function lose() {
+
   }
